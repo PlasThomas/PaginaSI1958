1101fc38b409375e3255858e3bc831ee8691616f
PlasThomas <joseplascencia346@aragon.unam.mx>
2025-09-10 20:45:48 -0600

    backend

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import bcrypt
 from flask_cors import CORS
 from pymongo import MongoClient
 from dotenv import load_dotenv
@@ -8,11 +9,21 @@ load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+try:
+    client = MongoClient(os.getenv("MONGO_URI"))
+    db = client['usuarios']
+    print('Conexion Exitosa.')
+except Exception as e:
+    print(f'ERROR: {e}')
 
-@app.route("/api/user", method=['GET'])
+@app.route("/api/login", method=['POST'])
 def login():
     pass
 
-@app.route("/api/user", method=['POST'])
+@app.route("/api/signup", method=['POST'])
 def signup():
+    pass
+
+@app.route("/api/logout", method=['POST'])
+def login():
     pass
